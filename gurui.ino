2acb13e5dac36cb05a82131b3331c4e9cffda32e
Dominik Lastovcic <dominik.lastovcic@gmail.com>
2023-07-29 01:10:32 +0200

    code refactor

--- gurui.ino
+++ gurui.ino
@@ -1,10 +1,11 @@
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// By Evandro Copercini - 2018
 //
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+// This example creates a bridge between Serial and Classical Bluetooth (SPP)
+// and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
 #include "BluetoothSerial.h"
+#include <Adafruit_NeoPixel.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -12,87 +13,74 @@
 
 BluetoothSerial SerialBT;
 
-
-#include <Adafruit_NeoPixel.h>
-
-
-//GPIO PIN 12
+// GPIO PIN 12
 #define PIN 4
 #define LED_COUNT 66
 
-
-int cnt = 0;
 int r = 0;
 int b = 0;
 int g = 0;
 char bt_serial;
-byte bajt;
-byte poruka[5];
-int br = 0;
-int pozicija;
+byte data;
+byte message[5];
+int cnt = 0;
+int pos;
 Adafruit_NeoPixel strip(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  SerialBT.begin("GURUITAR_TEST123"); //Bluetooth device name
+  SerialBT.begin("UKULEDLE"); // Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
-
   strip.begin(); // This initializes the NeoPixel library.
   strip.setBrightness(3);
   // put your setup code here, to run once:
-
-
-    
-
-
-
 }
 
-void loop() {
-  if (Serial.available()) {
+void loop()
+{
+  if (Serial.available())
+  {
     SerialBT.write(Serial.read());
   }
-  while (SerialBT.available() > 0) {
-    bajt = SerialBT.read();
-    //Serial.println(bajt);
-    poruka[br] = bajt;
-    br = br + 1;
-    //Serial.println(br);
-    if (br == 6) {
-      br = 0;
-      Serial.print( poruka[0]);
-      Serial.print( poruka[1]);
-      Serial.println( poruka[2]);
-      Serial.print( poruka[3]);
-      Serial.print( poruka[4]);
-      Serial.println( poruka[5]);
-      pozicija = poruka[1] + poruka [2]*4;
-      if (poruka[0] == 49 && poruka[1] == 0)
+  while (SerialBT.available() > 0)
+  {
+    data = SerialBT.read();
+    message[cnt] = data;
+    cnt = cnt + 1;
+    // Serial.println(cnt);
+    if (cnt == 6)
+    {
+      cnt = 0;
+      Serial.print(message[0]);
+      Serial.print(message[1]);
+      Serial.println(message[2]);
+      Serial.print(message[3]);
+      Serial.print(message[4]);
+      Serial.println(message[5]);
+      pos = message[1] + message[2] * 4;
+      if (message[0] == 49 && message[1] == 0)
       {
-        strip.setPixelColor(pozicija, strip.Color(255, 0, 0));
+        strip.setPixelColor(pos, strip.Color(255, 0, 0));
         strip.show();
-
       }
-            if (poruka[0] == 49 && poruka[1] == 1)
+      if (message[0] == 49 && message[1] == 1)
       {
-        strip.setPixelColor(pozicija, strip.Color(255, 255, 0));
+        strip.setPixelColor(pos, strip.Color(255, 255, 0));
         strip.show();
-
       }
-            if (poruka[0] == 49 && poruka[1] == 2)
+      if (message[0] == 49 && message[1] == 2)
       {
-        strip.setPixelColor(pozicija, strip.Color(0, 255, 0));
+        strip.setPixelColor(pos, strip.Color(0, 255, 0));
         strip.show();
-
       }
-            if (poruka[0] == 49 && poruka[1] == 3)
+      if (message[0] == 49 && message[1] == 3)
       {
-        strip.setPixelColor(pozicija, strip.Color(0, 0, 255));
+        strip.setPixelColor(pos, strip.Color(0, 0, 255));
         strip.show();
-
       }
 
-      if (poruka[0] == 51 && poruka[1] == 0)
+      if (message[0] == 51 && message[1] == 0)
       {
         strip.setPixelColor(0, strip.Color(255, 0, 0));
         strip.setPixelColor(4, strip.Color(255, 0, 0));
@@ -104,9 +92,8 @@ void loop() {
         strip.setPixelColor(28, strip.Color(255, 0, 0));
         strip.setPixelColor(32, strip.Color(255, 0, 0));
         strip.show();
-
       }
-            if (poruka[0] == 51 && poruka[1] == 1)
+      if (message[0] == 51 && message[1] == 1)
       {
         strip.setPixelColor(1, strip.Color(255, 255, 0));
         strip.setPixelColor(5, strip.Color(255, 255, 0));
@@ -118,9 +105,8 @@ void loop() {
         strip.setPixelColor(29, strip.Color(255, 255, 0));
         strip.setPixelColor(33, strip.Color(255, 255, 0));
         strip.show();
-
       }
-            if (poruka[0] == 51 && poruka[1] == 2)
+      if (message[0] == 51 && message[1] == 2)
       {
         strip.setPixelColor(2, strip.Color(0, 255, 0));
         strip.setPixelColor(6, strip.Color(0, 255, 0));
@@ -132,9 +118,8 @@ void loop() {
         strip.setPixelColor(30, strip.Color(0, 255, 0));
         strip.setPixelColor(34, strip.Color(0, 255, 0));
         strip.show();
-
       }
-            if (poruka[0] == 51 && poruka[1] == 3)
+      if (message[0] == 51 && message[1] == 3)
       {
         strip.setPixelColor(3, strip.Color(0, 0, 255));
         strip.setPixelColor(7, strip.Color(0, 0, 255));
@@ -146,20 +131,16 @@ void loop() {
         strip.setPixelColor(31, strip.Color(0, 0, 255));
         strip.setPixelColor(35, strip.Color(0, 0, 255));
         strip.show();
-
       }
-            
 
-      if (poruka[0] == 48)
+      if (message[0] == 48)
       {
-        strip.setPixelColor(pozicija, strip.Color(0, 0, 0));
+        strip.setPixelColor(pos, strip.Color(0, 0, 0));
         strip.show();
         delay(75);
-
       }
 
-      
-     if (poruka[0] == 52 && poruka[1] == 0)
+      if (message[0] == 52 && message[1] == 0)
       {
         strip.setPixelColor(0, strip.Color(0, 0, 0));
         strip.setPixelColor(4, strip.Color(0, 0, 0));
@@ -172,9 +153,8 @@ void loop() {
         strip.setPixelColor(32, strip.Color(0, 0, 0));
         strip.show();
         delay(75);
-
       }
-     if (poruka[0] == 52 && poruka[1] == 1)
+      if (message[0] == 52 && message[1] == 1)
       {
         strip.setPixelColor(1, strip.Color(0, 0, 0));
         strip.setPixelColor(5, strip.Color(0, 0, 0));
@@ -187,10 +167,9 @@ void loop() {
         strip.setPixelColor(33, strip.Color(0, 0, 0));
         strip.show();
         delay(75);
-
       }
-     
-     if (poruka[0] == 52 && poruka[1] == 2)
+
+      if (message[0] == 52 && message[1] == 2)
       {
         strip.setPixelColor(2, strip.Color(0, 0, 0));
         strip.setPixelColor(6, strip.Color(0, 0, 0));
@@ -202,11 +181,10 @@ void loop() {
         strip.setPixelColor(30, strip.Color(0, 0, 0));
         strip.setPixelColor(34, strip.Color(0, 0, 0));
         strip.show();
-       delay(75);
-
+        delay(75);
       }
 
-     if (poruka[0] == 52 && poruka[1] == 3)
+      if (message[0] == 52 && message[1] == 3)
       {
         strip.setPixelColor(3, strip.Color(0, 0, 0));
         strip.setPixelColor(7, strip.Color(0, 0, 0));
@@ -219,88 +197,78 @@ void loop() {
         strip.setPixelColor(35, strip.Color(0, 0, 0));
         strip.show();
         delay(75);
-
       }
 
-
-      if (poruka[0] == 13 && poruka[1] == 10 && poruka[2] == 10)
+      if (message[0] == 13 && message[1] == 10 && message[2] == 10)
       {
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++)
+        {
           strip.setPixelColor(i, strip.Color(255, 255, 255));
           strip.show();
           delay(20);
         }
 
-        for (int i = 6; i < 12; i++) {
+        for (int i = 6; i < 12; i++)
+        {
           strip.setPixelColor(i, strip.Color(255, 0, 0));
           strip.show();
           delay(20);
         }
 
-        for (int i = 12; i < 18; i++) {
+        for (int i = 12; i < 18; i++)
+        {
           strip.setPixelColor(i, strip.Color(255, 255, 0));
           strip.show();
           delay(20);
         }
 
-        for (int i = 18; i < 24; i++) {
+        for (int i = 18; i < 24; i++)
+        {
           strip.setPixelColor(i, strip.Color(0, 255, 0));
           strip.show();
           delay(20);
         }
 
-        for (int i = 24; i < 30; i++) {
+        for (int i = 24; i < 30; i++)
+        {
           strip.setPixelColor(i, strip.Color(0, 0, 255));
           strip.show();
           delay(20);
         }
-        for (int i = 30; i < 36; i++) {
+        for (int i = 30; i < 36; i++)
+        {
           strip.setPixelColor(i, strip.Color(255, 0, 255));
           strip.show();
           delay(20);
         }
 
-        for (int i = 0; i < 36; i++) {
+        for (int i = 0; i < 36; i++)
+        {
           strip.setPixelColor(i, strip.Color(0, 0, 0));
           strip.show();
           delay(50);
         }
-
       }
-        if (poruka[0] == 10 && poruka[1] == 10 && poruka[2] == 13)
+      if (message[0] == 10 && message[1] == 10 && message[2] == 13)
       {
 
-
-        for (int i = 0; i < 36; i++) {
+        for (int i = 0; i < 36; i++)
+        {
           strip.setPixelColor(i, strip.Color(0, 0, 0));
           strip.show();
           delay(50);
         }
-
       }
 
-        if (poruka[0] == 50)
+      if (message[0] == 50)
       {
 
-        strip.setPixelColor(pozicija, strip.Color(poruka[3]*2, poruka[4]*2, poruka[5]*2));
+        strip.setPixelColor(pos, strip.Color(message[3] * 2, message[4] * 2, message[5] * 2));
         strip.show();
-
       }
-
-
-
-
-
-
-
-
     }
-
   }
 
-  //SerialBT.write(bt_serial);
-
-
-
+  // SerialBT.write(bt_serial);
   delay(20);
 }
